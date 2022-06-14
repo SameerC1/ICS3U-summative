1e8231f378d13ba3ac26bad3b0b178938bce324e
SameerC1 <100710894+SameerC1@users.noreply.github.com>
2022-06-14 10:12:29 -0400

    Add files via upload
    Added collision detection to the enemy

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -4,43 +4,69 @@ const upmovement=Vector2(0,-1)
 const gravity=20
 const maxfallspeed=500
 const maxspeed=400
-const jumpspeed=600
+const jumpspeed=800
 const acceleration=10
+var health=3
 #variable to be used for moving
 var movement=Vector2()
 var facingright=true
-func _ready():
-		pass#TODO
+
 
 #will be used for the movement
 func _physics_process(_delta):
 	
 	#used to make gravity & speed limits
 	movement.y+=gravity
+	
 	movement.x=clamp(movement.x,-maxspeed,maxspeed)
 	if movement.y>maxfallspeed:
 		movement.y=maxfallspeed
 	
 	#used to make the sprite turn left & right
 	if facingright==true:
-		$body.scale.x=1
+		$playersprite.flip_h=false
 	else:
-		$body.scale.x=-1
+		$playersprite.flip_h=true
 	
 	#this is to move left & right
 	if Input.is_action_pressed("right"):
 		movement.x+=acceleration
 		facingright=true
+		$playersprite.play("moving")
 	elif Input.is_action_pressed("left"):
 		movement.x-=acceleration
 		facingright=false
+		$playersprite.play("moving")
 	else:
 		movement.x=lerp(movement.x,0,0.2)
-	
+		$playersprite.play("idle")
 	#lets you jump if you are on the floor	
 	if is_on_floor():
 		if Input.is_action_just_pressed("jump"):
 			movement.y=-jumpspeed
 	
+	#plays jump animation in the air
+	if not is_on_floor():
+		$playersprite.play("jump")
+	
 	#updates the movement
 	movement=move_and_slide(movement,upmovement)
+
+
+
+func _on_enemy_bounce_detector_area_shape_entered(area_id: int, area: Area2D, area_shape: int, local_shape: int) -> void:
+	movement.y=-jumpspeed*0.8
+	
+
+func _on_hit_detector_area_entered(area: Area2D) -> void:
+	self.hit(position.x)
+
+func hit(enemyposx):
+	set_modulate(Color(1,0.3,0.3,0.3))
+	health-=1
+	if position.x>enemyposx:
+		movement.x=-500
+	else:
+		movement.x=+500
+	movement.y=-jumpspeed*0.5
+	$playersprite.play("hurt")
--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,36 +1,43 @@
 extends KinematicBody2D
-var health=3
 var movement=Vector2()
 const gravity=20
-const maxspeed=200
+var speed=50
 const maxfallspeed=400
 const upmovement=Vector2(0,-1)
 var direction=1
-
+var hurt=false
 
 func ready():
 	$Enemybody.flip_h=false
 	$FloorChecker.position.x=$hitbox.shape.get_extents().x*direction
 
 func _physics_process(_delta):
-	#used to make gravity & speed limits
-	
-	movement.y+=gravity
-	if movement.y>maxfallspeed:
-		movement.y=maxfallspeed
+	if hurt==false:
+		#used to make gravity & speed limits
+		movement.y+=gravity
+		if movement.y>maxfallspeed:
+			movement.y=maxfallspeed
 
+		
+		#used to make the sprite turn left & right
+		#makes enemy turn & move
+		if is_on_wall() or not $FloorChecker.is_colliding() and is_on_floor():
+			direction=direction*-1
+			$Enemybody.flip_h=not $Enemybody.flip_h
+			$FloorChecker.position.x=$hitbox.shape.get_extents().x*direction
+		movement.x=speed*direction
+		movement=move_and_slide(movement,upmovement)
+		$Enemybody.play("moving")
 	
-	#used to make the sprite turn left & right
-	#makes enemy turn & move
-	if is_on_wall() or not $FloorChecker.is_colliding() and is_on_floor():
-		direction=direction*-1
-		$Enemybody.flip_h=not $Enemybody.flip_h
-		$FloorChecker.position.x=$hitbox.shape.get_extents().x*direction
-	movement.x=50*direction
-	movement=move_and_slide(movement,upmovement)
-	$Enemybody.play("moving")
-	
-#function for if it is stomped
-func _on_Area2D_body_entered(body: Node) -> void:
+	#function for if it is stomped
+
+
+func _on_hit_checker_area_shape_entered(area_id: int, area: Area2D, area_shape: int, local_shape: int) -> void:
+	hurt=true
+	set_modulate(Color(1,0.3,0.3,0.3))
+	speed=0
+	set_collision_layer_bit(1,false)
+	set_collision_mask_bit(0,false)
 	$Enemybody.play("hurt")
-
+	yield(get_tree().create_timer(0.5),"timeout")
+	get_tree().queue_delete(self)
